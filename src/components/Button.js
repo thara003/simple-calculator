0e6843909f67950bcef8e721b6abc331fc8b3754
Nayan Thara M <wittyhuminid@protonmail.com>
2022-01-23 22:38:47 +0530

    retrieve value from button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,27 @@
-import React from 'react'
-// impor
+import React from "react";
 
-function Button({color,value}) {
-    return (
-        <button style={{backgroundColor: color}} className='button'>{value}</button>
-    )
+
+function Button({ color, label }) {
+  const onclick = (e) => {
+    console.log(e.target.value);
+  };
+
+  return (
+
+    <input
+      type="button"
+      value={label}
+      style={{ backgroundColor: color }}
+      className="button"
+      onClick={onclick}
+    />
+  );
 }
 
-export default Button
+Button.defaultProps = {
+  color: "rgb(158, 255, 47)",
+};
+
+
+
+export default Button;
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -7,26 +7,26 @@ const Wrapper = () => {
         <div className = 'wrapper'>
            <Screen />
            <div className='keypad'>
-           <Button  color = 'red' value = "AC"></Button>
-           <Button  color = 'yellow' value = "C"></Button>
-           <Button  color = 'rgb(47, 255, 99)' value = "%"></Button>
-           <Button  color = 'rgb(47, 255, 99)' value = "/"></Button>
-           <Button  color = 'rgb(158, 255, 47)' value = "7"></Button>
-           <Button  color = 'rgb(158, 255, 47)' value = "8"></Button>
-           <Button  color = 'rgb(158, 255, 47)' value = "9"></Button>
-           <Button  color = 'rgb(47, 255, 99)' value = "x"></Button>
-           <Button  color = 'rgb(158, 255, 47)' value = "4"></Button>
-           <Button  color = 'rgb(158, 255, 47)' value = "5"></Button>
-           <Button  color = 'rgb(158, 255, 47)' value = "6"></Button>
-           <Button  color = 'rgb(47, 255, 99)' value = "+"></Button>
-           <Button  color = 'rgb(158, 255, 47)' value = "1"></Button>
-           <Button  color = 'rgb(158, 255, 47)' value = "2"></Button>
-           <Button  color = 'rgb(158, 255, 47)' value = "3"></Button>
-           <Button  color = 'rgb(47, 255, 99)' value = "-"></Button>
-           <Button  color = 'rgb(158, 255, 47)' value = "00"></Button>
-           <Button  color = 'rgb(158, 255, 47)' value = "0"></Button>
-           <Button  color = 'rgb(158, 255, 47)' value = "."></Button>
-           <Button  color = 'rgb(47, 255, 255)' value = "="></Button>
+           <Button  color = 'red' label={'AC'}></Button>
+           <Button  color = 'yellow' label={'C'}></Button>
+           <Button  color = 'rgb(47, 255, 99)' label={'%'}></Button>
+           <Button  color = 'rgb(47, 255, 99)' label={'/'}></Button>
+           <Button  color = 'rgb(158, 255, 47)' label={'7'}></Button>
+           <Button  color = 'rgb(158, 255, 47)' label={'8'}></Button>
+           <Button  color = 'rgb(158, 255, 47)' label={'9'}></Button>
+           <Button  color = 'rgb(47, 255, 99)' label={'x'}></Button>
+           <Button  color = 'rgb(158, 255, 47)' label={'4'}></Button>
+           <Button  color = 'rgb(158, 255, 47)' label={'5'}></Button>
+           <Button  color = 'rgb(158, 255, 47)' label={'6'}></Button>
+           <Button  color = 'rgb(47, 255, 99)' label={'+'}></Button>
+           <Button  color = 'rgb(158, 255, 47)' label={'1'}></Button>
+           <Button  color = 'rgb(158, 255, 47)' label={'2'}></Button>
+           <Button  color = 'rgb(158, 255, 47)' label={'3'}></Button>
+           <Button  color = 'rgb(47, 255, 99)' label={'-'}></Button>
+           <Button  color = 'rgb(158, 255, 47)' label={'00'}></Button>
+           <Button  color = 'rgb(158, 255, 47)' label={'0'}></Button>
+           <Button  color = 'rgb(158, 255, 47)' label={'.'}></Button>
+           <Button  color = 'rgb(47, 255, 255)' label={'='}></Button>
            </div>
         </div>
     )
